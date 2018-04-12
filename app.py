88eceecaa6a05acfd7d030419cd9820cf76ebe35
Bradford Muron <bradfordmuron@ool-ad020304.dyn.optonline.net>
2018-04-12 14:23:38 -0400

    Procfile4

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from random import randint
  
@@ -26,5 +27,10 @@ def index():
         'test.html',**locals())
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
+
+
+
+#app.run(host='0.0.0.0', port=80)
