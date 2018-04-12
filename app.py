7f8ae796beeaec7348061444de536495888d4e9c
Bradford Muron <bradfordmuron@ool-ad020304.dyn.optonline.net>
2018-04-12 15:18:36 -0400

    styling

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ if __name__ == "__main__":
 
 
 
+#top port deployment, bottom local testing
+#port = int(os.environ.get('PORT', 5000))
+#app.run(host='0.0.0.0', port=port)
 
 #app.run(host='0.0.0.0', port=80)
